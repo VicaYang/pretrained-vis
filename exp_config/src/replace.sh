16cffa48e4a885fffc981ae199381225776f730f
VicaYang <22126501+VicaYang@users.noreply.github.com>
2022-05-31 15:10:27 +0000

    add all 9 config and add replace

---
+++ exp_config/src/replace.sh
@@ -0,0 +1,3 @@
+VISSL_LIB_DIR="/home/vica/anaconda3/envs/vissl/lib/python3.8/site-packages/vissl"
+cp replace/logger.py ${VISSL_LIB_DIR}/utils/logger.py
+cp replace/checkpoint.py ${VISSL_LIB_DIR}/utils/checkpoint.py
